25a7aa7d4ecd87dda3039cca84e1214918dfa087
timursamanchi <timur.samanchi@gmail.com>
2023-12-01 15:48:04 +0000

    initial push

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "jenkins-vpc" {
   cidr_block = "10.0.0.0/16"
 
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     "Name" = "jenkins"
@@ -13,4 +13,19 @@ resource "aws_vpc" "jenkins-vpc" {
 }
 resource "aws_internet_gateway" "jenkins-igw" {
   vpc_id = aws_vpc.jenkins-vpc.id
+  tags = {
+    Name = "jenkins-igw"
+  }
+}
+
+resource "aws_route_table" "jenkins-route-table" {
+  vpc_id = aws_vpc.jenkins-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.jenkins-igw.id
+  }
+  tags = {
+    Name = "jenkins-route-table"
+  }
 }
