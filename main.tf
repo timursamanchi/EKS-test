1c1e11669b4151d6ad1fd985b2e082a52bbe8f86
timursamanchi <timur.samanchi@gmail.com>
2023-12-01 17:49:54 +0000

    initial push

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region = "eu-west-1"
 }
+
+# Main VPC
 resource "aws_vpc" "jenkins-vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -11,6 +13,8 @@ resource "aws_vpc" "jenkins-vpc" {
     "Name" = "jenkins"
   }
 }
+
+# VPC internet gateway
 resource "aws_internet_gateway" "jenkins-igw" {
   vpc_id = aws_vpc.jenkins-vpc.id
   tags = {
@@ -18,14 +22,32 @@ resource "aws_internet_gateway" "jenkins-igw" {
   }
 }
 
+# VPC route table
 resource "aws_route_table" "jenkins-route-table" {
   vpc_id = aws_vpc.jenkins-vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.jenkins-igw.id
-  }
   tags = {
     Name = "jenkins-route-table"
   }
 }
+
+# Public subnet
+resource "aws_subnet" "jenkins-PUB-1" {
+  vpc_id            = aws_vpc.jenkins-vpc.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "eu-west-1a"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name : "Jenkins-PUB-1"
+  }
+}
+
+# route table association with PUB subnet
+resource "aws_route_table_association" "namePUB-1-route-table-associationa" {
+  subnet_id = aws_subnet.jenkins-PUB-1.id
+  route_table_id = aws_route_table.jenkins-route-table.id
+}
+
+
